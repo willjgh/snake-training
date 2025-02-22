afc638d53f61eb7d0d2658979f066af1966d32ee
willjgh <williamjgh2000@gmail.com>
2025-02-22 19:57:48 +0000

    Testing evolution

--- test_evolution.py
+++ test_evolution.py
@@ -1,19 +1,23 @@
 from population import Population
 
 # create population of models
-population = Population(population_size=1000)
+population = Population(population_size=100)
 
 # initialize population
 population.initialize()
 
 # for each generation
-for gen in range(5):
+for gen in range(100):
 
     # compute fitness
     population.compute_fitness()
 
+    # display stats
+    population.print_population_statistics()
+
     # display best performance
-    population.display_fittest()
+    if gen % 10 == 0:
+        population.display_fittest()
 
     # evolve new generation
     population.evolve_population(selected_number=5)
